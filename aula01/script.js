0fbee7bda3bd45eab801efee5535656a837691a2
jpsoarescastro <jpsoaresdecastro@gmail.com>
2026-02-11 15:05:58 -0300

    ''

---
+++ aula01/script.js
@@ -0,0 +1,18 @@
+var nome = "João";
+
+var ida color: red;de = 67;
+
+var altura = 1.82;
+
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write("<h1>Ola, seja bem vindo ao JavaScript</h1>")
+
+document.write(nome + "<br>" );  
+document.write(idade + "<br>" );
+document.write(altura + "<br>" );
+
+document.write("<p style=' color: red'>" + "Voce ja viveu " + idade * 365 + " dias" + /p>"); 
